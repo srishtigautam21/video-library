6c4aec5811d8a42fb14a2b6874d681c64ae14f37
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-05-27 19:17:39 +0530

    Fix:commented code

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import "./navbar.css";
 import { UserIcon } from "../../Assets/allsvg";
 
 const Navbar = () => {
-  //   const [open, setOpen] = useState(false);
   return (
     <div className='navbar'>
       <nav className='nav-component nav-padding'>
@@ -14,25 +13,7 @@ const Navbar = () => {
           </Link>
         </div>
         <li className='icons-alignment'>
-          {/* <div className='dropdown'>
-            <Link
-              className='link ecom-link-color'
-              to='/homePage'
-              onClick={() => setOpen((visible) => !visible)}
-            > */}
           <UserIcon className='nav-icons' />
-          {/* </Link>
-            {open && (
-              <div className='dropdown-menu'>
-                <Link className='dropdown-content login-link' to='/login'>
-                  Login
-                </Link>
-                <Link className='dropdown-content profile' to='/homePage'>
-                  Profile
-                </Link>
-              </div>
-            )}
-          </div> */}
         </li>
       </nav>
     </div>
