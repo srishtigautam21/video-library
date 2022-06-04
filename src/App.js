d2e75b408120c58ba27de542abc2bc1ee764d89f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-06-04 12:54:12 +0530

    Add:react-youtube

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             }
           />
           <Route
-            path='/video/:videoId'
+            path='/video/:id'
             element={
               <RequireAuth>
                 <SingleVideoPage />
