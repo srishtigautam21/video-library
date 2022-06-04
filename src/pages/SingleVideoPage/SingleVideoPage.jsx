025aad2483c6fb8d30e8387d3e2aa4375374c3d2
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-06-04 12:53:29 +0530

    Add:videoPage

--- src/pages/SingleVideoPage/SingleVideoPage.jsx
+++ src/pages/SingleVideoPage/SingleVideoPage.jsx
@@ -1,13 +1,35 @@
+import { useParams, useLocation } from "react-router-dom";
 import YouTube from "react-youtube";
+import "./singleVideoPage.css";
+// import { videos } from "../../backend/db/videos";
 
-const SingleVideoPage = ({ _id }) => {
+const SingleVideoPage = () => {
+  const { id } = useParams();
+  // const {title, creator, thumbnail, creatorDp, views, _id}=videos;
+  const { state } = useLocation();
+  const video = state;
+  const videoOnReady = (event) => {
+    const player = event.target;
+
+    player.seekTo(0);
+  };
   const opts = {
-    height: "390",
-    width: "640",
+    height: "450", //390
+    width: "700", //640
     playerVars: {
       autoplay: 0,
     },
   };
-  return <YouTube videoId={_id} opts={opts} onReady={this._onReady} />;
+  return (
+    <div className='video-conatiner'>
+      <YouTube videoId={id} opts={opts} onReady={videoOnReady} />
+      <div className='videoPage-title'>
+        <h3>{video.title}</h3>
+        <div className='video-operation'>
+          <p>{video.views}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export { SingleVideoPage };
