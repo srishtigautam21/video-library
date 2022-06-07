4bf6fb2c3c5703cc9f7041d2ce9a716f1097a2bb
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-06-07 08:20:26 +0530

    Fix: Require auth

--- src/App.js
+++ src/App.js
@@ -24,38 +24,12 @@ function App() {
           <Route path='/explorePage' element={<VideoListingPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/video/:id' element={<SingleVideoPage />} />
-          <Route
-            path='/playlist'
-            element={
-              <RequireAuth>
-                <PlaylistPage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path='/watchlater'
-            element={
-              <RequireAuth>
-                <WatchLaterPage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path='/liked'
-            element={
-              <RequireAuth>
-                <LikedVideosPage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path='/history'
-            element={
-              <RequireAuth>
-                <HistoryPage />
-              </RequireAuth>
-            }
-          />
+          <Route element={<RequireAuth />}>
+            <Route path='/playlist' element={<PlaylistPage />} />
+            <Route path='/watchlater' element={<WatchLaterPage />} />
+            <Route path='/liked' element={<LikedVideosPage />} />
+            <Route path='/history' element={<HistoryPage />} />
+          </Route>
         </Routes>
       </div>
     </>
