44d1be6cbcd74579938f0db6a8f1fd21cdf631fc
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-06-29 12:42:22 +0530

    react player to iframe

--- src/pages/SingleVideoPage/SingleVideoPage.jsx
+++ src/pages/SingleVideoPage/SingleVideoPage.jsx
@@ -1,6 +1,6 @@
 import { useParams, useLocation } from "react-router-dom";
 // import YouTube from "react-youtube";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 import "./singleVideoPage.css";
 import { useWatchLater, useModal } from "../../context";
 import { PlayListModal, Footer } from "../../component";
@@ -30,27 +30,26 @@ const SingleVideoPage = () => {
   //   player.seekTo(0);
   // };
   // const opts = {
-  // height: "450",
-  // width: "700",
-  //   height: "100%",
-  //   width: "100%",
+  //   height: "450",
+  //   width: "700",
   //   playerVars: {
   //     autoplay: 1,
   //   },
   // };
-  const ResponsivePlayer = ({ id }) => {
-    return (
-      <div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url={`https://www.youtube.com/watch?v=${id}`}
-          width='100%'
-          height='100%'
-          controls={true}
-        />
-      </div>
-    );
-  };
+  // const ResponsivePlayer = ({ id }) => {
+  //   return (
+  //     <div className='player-wrapper'>
+  //     <ReactPlayer
+  //       className='react-player'
+  //       url={`https://www.youtube.com/watch?v=${id}`}
+  //       width='100%'
+  //       height='100%'
+
+  //       controls={true}
+  //     />
+  //    </div>
+  //   );
+  // };
 
   const isLiked = likedVideoList.findIndex((like) => like._id === video._id);
   const isInWatchLater = watchLaterList.findIndex(
@@ -61,9 +60,19 @@ const SingleVideoPage = () => {
   };
   return (
     <>
-      <div className='videoPage-wrapper'>
-        <div className='video-container'>
-          {/* <div className='player-wrapper'>
+      {/* <div className='videoPage-wrapper'> */}
+      <div className='video-container'>
+        <div className='iframe-container'>
+          <iframe
+            src={`https://www.youtube.com/embed/${id}?autoplay=1&modestbranding=1&rel=0&iv_load_policy=3`}
+            className='player'
+            title={video.title}
+            frameBorder='0'
+            allowFullScreen
+            allow='accelerometer; clipboard-write; encrypted-media; gyroscope'
+          ></iframe>
+        </div>
+        {/* <div className='player-wrapper'>
             <YouTube
               videoId={id}
               opts={opts}
@@ -71,92 +80,97 @@ const SingleVideoPage = () => {
               className='react-player'
             />
           </div> */}
-          <div className='single-video-player'>
-            <ResponsivePlayer id={id} />
-          </div>
+        {/* <div className='single-video-player'>
+          <ResponsivePlayer id={id} />
+        </div> */}
 
-          {/* <YouTube videoId={id} opts={opts} onReady={videoOnReady} /> */}
-          <div className='video-content-container'>
-            <div className='videoPage-title'>{video.title}</div>
-            <div className='video-operation'>
-              <p>{video.views} views</p>
-              <div className='youtube-icons'>
-                <div className='video-item'>
-                  {isLiked === -1 ? (
-                    <button
-                      className='video-btn'
-                      onClick={() => addToLikedVideo(video)}
-                    >
-                      <span>
-                        <LikeIcon className='incr-font' />
-                      </span>
-                      <p className='incr-font'>Like</p>
-                    </button>
-                  ) : (
-                    <button
-                      className='video-btn'
-                      onClick={() => unlikeVideoHandler(video._id)}
-                    >
-                      <span>
-                        <LikeIconFilled className='incr-font' />
-                      </span>
-                      <p className='incr-font video-icon-color'>Like</p>
-                    </button>
-                  )}
-                </div>
-                <div className='video-item'>
-                  {isInWatchLater === -1 ? (
-                    <button
-                      className='video-btn'
-                      onClick={() => watchLaterHandler(video)}
-                    >
-                      <span>
-                        <ClockIcon className='incr-font' />
-                      </span>
-                      <p className='incr-font'>Watch Later</p>
-                    </button>
-                  ) : (
-                    <button className='video-btn btn-disable' disabled>
-                      <span>
-                        <ClockIcon className='incr-font' />
-                      </span>
-                      <p className='incr-font'>Watch Later</p>
-                    </button>
-                  )}
-                </div>
-                <div className='video-item'>
+        {/* <YouTube
+            className='player'
+            videoId={id}
+            opts={opts}
+            onReady={videoOnReady}
+          /> */}
+        <div className='video-content-container'>
+          <div className='videoPage-title'>{video.title}</div>
+          <div className='video-operation'>
+            <p>{video.views} views</p>
+            <div className='youtube-icons'>
+              <div className='video-item'>
+                {isLiked === -1 ? (
                   <button
                     className='video-btn'
-                    onClick={() => setOpenModal(true)}
+                    onClick={() => addToLikedVideo(video)}
                   >
                     <span>
-                      <PlaylistIcon className='incr-font' />
+                      <LikeIcon className='incr-font' />
                     </span>
-                    <p className='incr-font'> Save</p>
+                    <p className='incr-font'>Like</p>
                   </button>
-                </div>
+                ) : (
+                  <button
+                    className='video-btn'
+                    onClick={() => unlikeVideoHandler(video._id)}
+                  >
+                    <span>
+                      <LikeIconFilled className='incr-font' />
+                    </span>
+                    <p className='incr-font video-icon-color'>Like</p>
+                  </button>
+                )}
               </div>
-              <PlayListModal
-                setOpenModal={setOpenModal}
-                openModal={openModal}
-                video={video}
+              <div className='video-item'>
+                {isInWatchLater === -1 ? (
+                  <button
+                    className='video-btn'
+                    onClick={() => watchLaterHandler(video)}
+                  >
+                    <span>
+                      <ClockIcon className='incr-font' />
+                    </span>
+                    <p className='incr-font'>Watch Later</p>
+                  </button>
+                ) : (
+                  <button className='video-btn btn-disable' disabled>
+                    <span>
+                      <ClockIcon className='incr-font' />
+                    </span>
+                    <p className='incr-font icon-hide'>Watch Later</p>
+                  </button>
+                )}
+              </div>
+              <div className='video-item'>
+                <button
+                  className='video-btn'
+                  onClick={() => setOpenModal(true)}
+                >
+                  <span>
+                    <PlaylistIcon className='incr-font' />
+                  </span>
+                  <p className='incr-font icon-hide'> Save</p>
+                </button>
+              </div>
+            </div>
+            <PlayListModal
+              setOpenModal={setOpenModal}
+              openModal={openModal}
+              video={video}
+            />
+          </div>
+          <div className='video-info'>
+            <div className='video-creator-name'>
+              <img
+                className='video-creator-round-img'
+                src={video.creatorDp}
+                alt={video.creator}
               />
+              <div className='incr-font-creator'>{video.creator}</div>
             </div>
-            <div className='video-info'>
-              <div className='video-creator-name'>
-                <img
-                  className='video-creator-round-img'
-                  src={video.creatorDp}
-                  alt={video.creator}
-                />
-                <div className='incr-font-creator'>{video.creator}</div>
-              </div>
-              {/* Future TODOs:will add the video description */}
-              {/* <div className='video-discription'>{video.description}</div> */}
-            </div>
+
+            <div className='video-discription'>{video.description}</div>
           </div>
         </div>
       </div>
+      {/* </div> */}
       <div className='footer footer-mediaquery'>
         <Footer />
       </div>
